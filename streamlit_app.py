d916a062f53b1d67ec1b5155fba8e5eb66c7fae0
danil1933 <danialazkaham@gmail.com>
2025-05-27 09:36:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Danialazka Web")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "dunia boleh saja melawanku ku punya doa ibu #perunggu"
 )
+st.image("IMG_3178.webp",width=200)
+
+st.title("aplikasi sederhana")
+st.header("aplikasi mengecek nilai ganjil/genap")
+angka = st.number_input("tulis sebuah angka:",value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah bilangan genap")
+else:
+    st.write(f"{angka} adalah bilangan ganjil)
